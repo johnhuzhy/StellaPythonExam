c3b6a91e794dfb36727360e099a19eb381ed4752
johnhuzhy <johnho560@gmail.com>
2020-06-05 22:52:21 +0900

    06/05 commit

--- src/practiceList/l3.py
+++ src/practiceList/l3.py
@@ -10,3 +10,17 @@ mult=[[1,2,3],['a','b','c'],'d','e',{5,6,7,8},(9,10)]
 print(mult[3][0]) #e
 # print(mult[4][3]) #TypeError: 'set' object is not subscriptable 集合是没有下标的，不能索引和切片 无序
 print(mult[5][1]) #10
+
+#字符串和列表的转换 字符串转列表用split 列表转字符串用jion
+str1='I love Java and python '
+str2=str1.split() #不写按什么分割时默认按空格分割
+print(str2) #['I', 'love', 'Java', 'and', 'python']
+str3='Java,Python,DB,Janpness'
+str3=str3.split(',')
+print(str3) #['Java', 'Python', 'DB', 'Janpness']
+#用*来拼接列表
+alst=['123','DB','77','Java','88']
+alst_to_str='*'.join(alst)
+print(alst_to_str) #123*DB*77*Java*88
+
+
---
+++ src/practiceTest/t2.py
@@ -0,0 +1,55 @@
+# 加减乘除
+
+print("请输入第一个整数:")
+a = input()
+a = int(a)
+print("请输入第二个整数:")
+b = input()
+b = int(b)
+
+# 加
+
+
+def add(a, b):
+    print("{0}和{1}相加、結果：{2}".format(a, b, a+b))
+
+
+# sub
+def sub(a, b):
+    print("{0}和{1}相减、結果：{2}".format(a, b, a-b))
+
+# 乘积
+
+
+def chengji(a, b):
+    print("{0}和{1}乘积、結果：{2}".format(a, b, a*b))
+
+# 除
+
+
+def chu(a, b):
+    if(b == 0):
+        print("除数为0，不能相除")
+    else:
+        print("{0}和{1}相除、結果：{2}".format(a, b, a/b))
+
+# 取余
+
+
+def quyu(a, b):
+    if(b == 0):
+        print("除数为0，不能相除")
+    else:
+        print("{0}和{1}取余、結果：{2}".format(a, b, a%b))
+
+# 咪
+def mici(a, b):
+    print("{0}和{1}幂次、結果：{2}".format(a, b, a**b))
+
+
+add(a, b)
+sub(a, b)
+chengji(a, b)
+chu(a, b)
+quyu(a, b)
+mici(a, b)
